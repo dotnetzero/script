3d908610400eb74b74650520be6c0d54525a211d
Eric Williams <ericwilliams76@gmail.com>
2016-12-01 19:13:01 -0700

    Adds parallel build switch
    Fixes #11

--- components/script-tasks/Plain-Compile.ps1
+++ components/script-tasks/Plain-Compile.ps1
@@ -1,7 +1,8 @@
-task Compile -depends Init,#Restore-Packages,#Create-AssemblyInfo {
+task Compile -depends Init, Restore-Packages, Create-AssemblyInfo {
     Exec {
         msbuild $solutionFile `
             /verbosity:$verbosity `
+            /m `
             /p:Configuration=$buildConfiguration `
             /p:Platform=$buildPlatform
     }
